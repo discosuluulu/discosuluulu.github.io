7521c676a437334e7df5437ee997a14a1f17efbf
discosuluulu <discosuluulu@gmail.com>
2025-07-15 12:49:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+document.getElementById('exploreBtn').addEventListener('click', () => {
+  document.getElementById('audioPlayerContainer').classList.remove('hidden');
+  initializePlayer(); // Defined in audioPlayer.js
+});
